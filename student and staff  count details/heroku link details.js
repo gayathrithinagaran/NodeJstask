aa3c585efc4ef391e6a5261fcb3989ef570999f8
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-07-13 20:33:21 +0530

    Create heroku link details.js

---
+++ student and staff  count details/heroku link details.js
@@ -0,0 +1,22 @@
+https://studentdetailandstaffdetails.herokuapp.com/
+
+//staff
+{
+    "id":1,
+    "name":"anu"
+}
+{
+    "id":2,
+    "name":"raj"
+}
+//student
+{
+    "id":1,
+    "name":"farhana",
+    "staffid":1
+}
+{
+    "id":2,
+    "name":"harish",
+    "staffid":"2"
+}
